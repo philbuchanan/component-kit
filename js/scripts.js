6f65cfd0e5b7cba1476385b25a5aee1c4adc1b71
Phil Buchanan <info@philbuchanan.com>
2018-05-12 13:44:09 -0400

    #6 Added ability for modals to self register in JS

--- js/scripts.js
+++ js/scripts.js
@@ -9,34 +9,43 @@
 	 * Modal Class
 	 */
 
-	var Modal = function(options) {
+	var Modal = function(modal) {
 		var _self = this;
 
-		this.options = options;
+		var classes = {};
+
+		Object.assign(classes, modal.dataset);
+
+		this.components = {
+			modal:      modal,
+			openModal:  document.querySelectorAll('.' + classes.openModal),
+			closeModal: document.querySelectorAll('.' + classes.closeModal),
+		};
+
 		this.state = {
 			open: false
 		};
 
 		this.openModal = function() {
-			_self.options.modal.classList.add('is-open');
+			_self.components.modal.classList.add('is-open');
 			_self.state.open = true;
 		};
 
 		this.closeModal = function() {
-			_self.options.modal.classList.add('is-closing');
-			_self.options.modal.classList.remove('is-open');
+			_self.components.modal.classList.add('is-closing');
+			_self.components.modal.classList.remove('is-open');
 
 			setTimeout(function() {
-				_self.options.modal.classList.remove('is-closing');
+				_self.components.modal.classList.remove('is-closing');
 				_self.state.open = false;
 			}, duration);
 		};
 
-		Array.from(_self.options.openModal).forEach(button => {
+		Array.from(_self.components.openModal).forEach(button => {
 			button.addEventListener('click', _self.openModal);
 		});
 
-		Array.from(_self.options.closeModal).forEach(button => {
+		Array.from(_self.components.closeModal).forEach(button => {
 			button.addEventListener('click', _self.closeModal);
 		});
 
@@ -50,53 +59,9 @@
 		});
 	};
 
-
-
-	/**
-	 * Set up individual modals
-	 */
-
-	var regularModal = new Modal({
-		modal:      document.querySelector('.js-modal'),
-		openModal:  document.querySelectorAll('.js-open-modal'),
-		closeModal: document.querySelectorAll('.js-close-modal')
-	});
-
-	var twofactorModal = new Modal({
-		modal:      document.querySelector('.js-2fa-modal'),
-		openModal:  document.querySelectorAll('.js-open-2fa-modal'),
-		closeModal: document.querySelectorAll('.js-close-2fa-modal')
-	});
-
-	var formModal = new Modal({
-		modal:      document.querySelector('.js-form-modal'),
-		openModal:  document.querySelectorAll('.js-open-form-modal'),
-		closeModal: document.querySelectorAll('.js-close-form-modal')
-	});
-
-
-
-	/**
-	 * Delete confirmation
-	 *
-	 * Forces a user to type 'DELETE' in order to continue.
-	 */
-
-	var deleteModal = new Modal({
-		modal:      document.querySelector('.js-delete-modal'),
-		openModal:  document.querySelectorAll('.js-open-delete-modal'),
-		closeModal: document.querySelectorAll('.js-close-delete-modal')
-	});
-
-	var deleteButton = document.querySelector('.js-delete-button');
-
-	document.querySelector('.js-delete-input').addEventListener('keyup', function(event) {
-		if (this.value === 'DELETE') {
-			deleteButton.removeAttribute('disabled');
-		}
-		else {
-			deleteButton.setAttribute('disabled', 'disabled');
-		}
+	// Enable all dropdowns
+	Array.from(document.querySelectorAll('.js-modal')).forEach(modal => {
+		new Modal(modal);
 	});
 
 
@@ -108,10 +73,11 @@
 	var Dropdown = function(btn) {
 		var _self = this;
 
-		this.options = {
-			trigger: btn,
+		this.components = {
+			trigger:  btn,
 			dropdown: btn.nextElementSibling
 		};
+
 		this.state = {
 			open: false
 		};
@@ -126,23 +92,23 @@
 		};
 
 		this.openDropdown = function() {
-			_self.options.trigger.classList.add('is-open');
-			_self.options.dropdown.classList.add('is-open');
+			_self.components.trigger.classList.add('is-open');
+			_self.components.dropdown.classList.add('is-open');
 			_self.state.open = true;
 		};
 
 		this.closeDropdown = function() {
-			_self.options.trigger.classList.remove('is-open');
-			_self.options.dropdown.classList.remove('is-open');
-			_self.options.dropdown.classList.add('is-closing');
+			_self.components.trigger.classList.remove('is-open');
+			_self.components.dropdown.classList.remove('is-open');
+			_self.components.dropdown.classList.add('is-closing');
 
 			setTimeout(function() {
-				_self.options.dropdown.classList.remove('is-closing');
+				_self.components.dropdown.classList.remove('is-closing');
 				_self.state.open = false;
 			}, duration);
 		};
 
-		_self.options.trigger.addEventListener('click', function(event) {
+		_self.components.trigger.addEventListener('click', function(event) {
 			event.stopPropagation();
 
 			_self.toggleDropdown();
